d6514305cd1de0b15a957fee619b96256dca7af8
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-08-08 14:19:11 +0530

    Embedded the slide extractor to the flask app

---
+++ app/contentManagement.py
@@ -0,0 +1,43 @@
+import os
+import win32com.client
+
+def Slide_Extractor():
+	slide_directory = os.listdir('C:/Users/Abhishek/Desktop/displayPpt')
+	print slide_directory
+	ppt_list = []
+	for ppt in slide_directory:
+		if ppt.endswith('.pptx') or ppt.endswith('.ppt'):
+			ppt_list.append(ppt)
+	print ppt_list
+
+	Application = win32com.client.Dispatch("PowerPoint.Application")
+	Application.Visible = True
+
+	slide_count = 0
+	old_count = 0
+
+	for element in ppt_list:
+		Presentation = Application.Presentations.Open(r'C:/Users/Abhishek/Desktop/displayPpt/'+element)
+
+		slide_count = slide_count + len(Presentation.Slides)
+		print (slide_count)
+
+		for c in range(old_count, slide_count):
+			Presentation.Slides[c-old_count].Export(r'C:\Users\Abhishek\Desktop\displayPpt\app\static\images\image{0}.jpg'.format(c), "JPG", 1366, 768)
+
+		old_count = slide_count
+
+	Application.Quit()
+
+	return slide_count
+
+def Content():
+	
+	IMAGE_LIST = []
+	count = Slide_Extractor()
+	for i in range(0,count):
+		IMAGE_LIST.append("image{0}.jpg".format(i))
+
+	print(IMAGE_LIST)
+
+	return IMAGE_LIST
