b0801d48f4af5493bec3b47a3f82014bd55d1acf
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-09-03 02:26:12 +0530

    Handled admin login

--- app/__init__.py
+++ app/__init__.py
@@ -14,9 +14,34 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
 	return render_template("index.html", image_list=image_list)
 
+@app.route('/login/index/')
+def back_to_index():
+	return redirect(url_for("index"))
+
 @app.route('/media/<filename>')
 def uploaded_file(filename):
 	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/login/', methods = ['GET','POST'])
+def loginpage():
+	error = None
+	try:
+		print ("in try")
+		if request.method == "POST":
+			attempted_username = request.form['username']
+			attempted_password = request.form['password']
+
+			if attempted_username == "admin" and attempted_password == "password":
+				return redirect(url_for("adminPanel"))
+			else:
+				error = "Invalid Credentials. Try Again."
+				
+		return render_template("login.html", error=error)
+
+	except Exception as e:
+		print("in except")
+		return render_template("login.html", error=error)
+
 
 if __name__ == '__main__':
 	# app.run(host='192.168.43.108')
