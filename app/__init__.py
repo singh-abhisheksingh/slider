5a6fcfc658648975e21ff9095370b4c7a506ac9d
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-09-06 11:11:32 +0530

    Handled page routes after delete and upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template
 from flask import request, url_for, redirect, session
 from flask import send_from_directory
+from werkzeug import secure_filename
+import os
 
-from contentManagement import Content, Display
+from contentManagement import Content, Display, Delete
 
-UPLOAD_FOLDER = './media'
+IMAGE_UPLOAD_FOLDER = './media'
+PPT_UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['ppt', 'pptx'])
 image_list = Content()
-ppt_list = Display()
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = IMAGE_UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = PPT_UPLOAD_FOLDER
 
 @app.route('/logout/')
 @app.route('/')
 def index():
+	# image_list = Content()
 	session['logged_in'] = False
 	return render_template("index.html", image_list=image_list)
 
@@ -25,7 +30,7 @@ def back_to_index():
 	return redirect(url_for("index"))
 
 @app.route('/media/<filename>')
-def uploaded_file(filename):
+def uploaded_image_file(filename):
 	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/login/', methods = ['GET','POST'])
@@ -53,11 +58,42 @@ def loginpage():
 @app.route('/adminPanel/')
 def adminPanel():
 	if (session['logged_in'] == True):
+		ppt_list = Display()
 		return render_template("adminPanel.html", PPT_LIST=ppt_list)
 	else:
 		return "Kindly log in"
 
+@app.route('/delete/', methods = ['GET','POST'])
+def deletePpt():
+	if request.method == "POST":
+		print(request.form['deleteIt'])
+		Delete(request.form['deleteIt'])
+		return redirect(url_for("adminPanel"))
+	else:
+		return redirect(url_for("adminPanel"))
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload/', methods = ['GET', 'POST'])
+def upload_file():
+	if request.method == "POST":
+		if 'file' not in request.files:
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			Content()
+			return redirect(url_for("adminPanel"))
+			# return redirect(url_for('uploaded_ppt_file', filename=filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_ppt_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 if __name__ == '__main__':
 	# app.run(host='192.168.12.155')
-
 	app.run(debug=True)
